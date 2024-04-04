d8d7047286fc3e8f18755c2d8d14bcf703c7e1d1
imsagar07 <sagarfale8@gmail.com>
2024-04-04 07:03:07 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DynamicFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-dynamic-form',
+  templateUrl: './dynamic-form.component.html',
+  styleUrls: ['./dynamic-form.component.scss']
+})
+export class DynamicFormComponent implements OnInit {
+
+  form: FormGroup;
+  jsonData = [ 
+    { ControlName: 'FirstName', Position: 1, IsRequired: true },
+    { ControlName: 'LastName', Position: 2, IsRequired: false },
+    { ControlName: 'ContactNo', Position: 3, IsRequired: true },
+    { ControlName: 'Email', Position: 4, IsRequired: false },
+    { ControlName: 'Address', Position: 5, IsRequired: false }
+  ]; 
+  tableData: any[] = []; 
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({}); 
+    this.jsonData.forEach(field => { 
+      this.form.addControl(field.ControlName.toLowerCase(), 
+        new FormControl('', field.IsRequired ? Validators.required : null));
+    });
+  }
+
+  saveData(): void {
+    if (this.form.valid) {
+      this.tableData.push(this.form.value);
+      console.log(this.tableData)
+      this.form.reset();
+    } else {
+      alert('Please fill in all required fields.');
+    }
+  }
+
+  clearForm(): void {
+    this.form.reset();
+  }
+}
